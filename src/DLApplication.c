efc77e8bc1a8f10e76b662c6b1fdbd26e7c443c5
dx <avi.cpp@gmail.com>
2011-02-04 00:13:59 +0200

    Root controller: DLApplication; Signals' callbacks encapsulations

--- src/DLApplication.c
+++ src/DLApplication.c
@@ -3,20 +3,28 @@
 #include <gio/gio.h>
 
 // Real virtual method prototypes
-
 void dl_application_run_real(DLApplication *self);
 
-// Private
+// Signals' callbacks
+G_MODULE_EXPORT void dl_application_download_button_clicked(gpointer selfRef, GtkObject *gtkObject);
+
+// Private methods
+void dl_application_perform_download(DLApplication *self);
+void dl_application_download_progress(goffset currentNumBytes, goffset totalNumBytes, gpointer selfRef);
+void dl_application_download_finished(GObject *sourceObject, GAsyncResult *result, gpointer selfRef);
+
+// Private defenitions
 #define DL_APPLICATION_GET_PRIVATE(obj)      (G_TYPE_INSTANCE_GET_PRIVATE ((obj), DL_TYPE_APPLICATION, DLApplicationPrivate))
 
 struct _DLApplicationPrivate
 {
+    GtkWidget *mainWindow;
     GtkWidget *urlEntry;
+    GtkWidget *downloadButton;
     GtkWidget *progressBar;
 };
 
 // Class constructor
-
 static void dl_application_class_init(DLApplicationClass *klass)
 {
     GObjectClass *gobjectClass = G_OBJECT_CLASS(klass);
@@ -36,6 +44,22 @@ static void dl_application_init(DLApplication *self)
     self->priv = priv;
     
     // init private members
+    GtkBuilder *builder = gtk_builder_new();
+    GError *error = NULL;
+    gtk_builder_add_from_file(builder, "resources/DLMainWindow.glade", &error);
+    gtk_builder_connect_signals(builder, NULL);
+
+    self->priv->mainWindow = GTK_WIDGET(gtk_builder_get_object(builder, "mainWindow"));
+    self->priv->urlEntry = GTK_WIDGET(gtk_builder_get_object(builder, "urlEntry"));
+    self->priv->downloadButton = GTK_WIDGET(gtk_builder_get_object(builder, "downloadButton"));
+    self->priv->progressBar = GTK_WIDGET(gtk_builder_get_object(builder, "downloadProgressBar"));
+
+    g_signal_connect_swapped(self->priv->downloadButton, "clicked",
+        G_CALLBACK(dl_application_download_button_clicked), self);
+    
+    gtk_progress_bar_set_fraction(GTK_PROGRESS_BAR(self->priv->progressBar), 0.0f);
+
+    g_object_unref(G_OBJECT(builder));
 }
 
 DLApplication *dl_application_new()
@@ -55,19 +79,7 @@ void dl_application_run(DLApplication *self)
 
 void dl_application_run_real(DLApplication *self)
 {
-    GtkBuilder *builder = gtk_builder_new();
-    GError *error = NULL;
-    gtk_builder_add_from_file(builder, "resources/DLMainWindow.glade", &error);
-    gtk_builder_connect_signals(builder, NULL);
-
-    GtkWidget *mainWindow = GTK_WIDGET(gtk_builder_get_object(builder, "mainWindow"));
-    self->priv->urlEntry = GTK_WIDGET(gtk_builder_get_object(builder, "urlEntry"));
-    self->priv->progressBar = GTK_WIDGET(gtk_builder_get_object(builder, "downloadProgressBar"));
-    gtk_progress_bar_set_fraction(GTK_PROGRESS_BAR(self->priv->progressBar), 0.0f);
-
-    g_object_unref(G_OBJECT(builder));
-
-    gtk_widget_show(mainWindow); 
+    gtk_widget_show(self->priv->mainWindow); 
 }
 
 
@@ -87,4 +99,35 @@ void dl_application_dispose(GObject *gobject)
         self->priv->progressBar = NULL;
     }
     G_OBJECT_CLASS(dl_application_parent_class)->dispose(gobject);
+}
+
+// Signals' callbacks
+G_MODULE_EXPORT void dl_application_download_button_clicked(gpointer selfRef, GtkObject *gtkObject)
+{
+    DLApplication *self = DL_APPLICATION(selfRef);
+    dl_application_perform_download((self));
+}
+
+void dl_application_perform_download(DLApplication *self)
+{
+    GFile *inFile = g_file_new_for_uri(gtk_entry_get_text(GTK_ENTRY(self->priv->urlEntry)));
+    char *inFileBasename = g_file_get_basename(inFile);
+    GFile *outFile = g_file_new_for_path(inFileBasename);
+
+    g_file_copy_async(inFile, outFile,
+        G_FILE_COPY_BACKUP, G_PRIORITY_DEFAULT, NULL,
+        dl_application_download_progress, self,
+        dl_application_download_finished, self);
+}
+void dl_application_download_progress(goffset currentNumBytes, goffset totalNumBytes, gpointer selfRef)
+{
+    DLApplication *self = DL_APPLICATION(selfRef);
+
+    gtk_progress_bar_set_fraction(GTK_PROGRESS_BAR(self->priv->progressBar), (gdouble)currentNumBytes / (gdouble)totalNumBytes);
+    g_print("\rProgress: %f%%",(gdouble)((gdouble)currentNumBytes) / (gdouble)totalNumBytes * 100);
+}
+
+void dl_application_download_finished(GObject *sourceObject, GAsyncResult *result, gpointer selfRef)
+{
+    g_print("\nDownloading is finished\n");
 }
--- src/main.c
+++ src/main.c
@@ -4,107 +4,14 @@
 
 // TODO release data before quit application
 
-// Some global var used as class's members as if the main function were application controller
-GtkWidget *gUrlEntry = NULL;
-GtkWidget *gProgressBar = NULL;
-
-// Signals
-G_MODULE_EXPORT void download_button_clicked(GtkObject *gtkObject, gpointer userData);
-
-// Downloading
-void perform_download();
-void download_progress(goffset currentNumBytes, goffset totalNumBytes, gpointer userData);
-void download_finished(GObject *sourceObject, GAsyncResult *result, gpointer userData);
-
-// Different download mechanisms
-void perform_download_by_reading();
-void perform_download_by_copying();
-
 int main(int argc, char *argv[])
 {
     gtk_init(&argc, &argv);
 
-    //DLApplication *application = dl_application_new();
-    //dl_application_run(application);
-
-    GtkBuilder *builder = gtk_builder_new();
-    GError *error = NULL;
-    gtk_builder_add_from_file(builder, "resources/DLMainWindow.glade", &error);
-    gtk_builder_connect_signals(builder, NULL);
-
-    GtkWidget *mainWindow = GTK_WIDGET(gtk_builder_get_object(builder, "mainWindow"));
-    gUrlEntry = GTK_WIDGET(gtk_builder_get_object(builder, "urlEntry"));
-    gProgressBar = GTK_WIDGET(gtk_builder_get_object(builder, "downloadProgressBar"));
-    gtk_progress_bar_set_fraction(GTK_PROGRESS_BAR(gProgressBar), 0.0f);
-
-    g_object_unref(G_OBJECT(builder));
-
-    gtk_widget_show(mainWindow); 
+    DLApplication *application = dl_application_new();
+    dl_application_run(application);
 
     gtk_main();
 
     return 0;
-}
-
-// Signals
-G_MODULE_EXPORT void download_button_clicked(GtkObject *gtkObject, gpointer userData)
-{
-    perform_download();
-}
-
-// Downloading
-void perform_download()
-{
-    //perform_download_by_reading();
-    perform_download_by_copying();
-}
-
-// Different download mechanisms
-void perform_download_by_reading()
-{
-    GError *error = NULL;
-
-    GFile *inFile = g_file_new_for_uri(gtk_entry_get_text(GTK_ENTRY(gUrlEntry)));
-    GFileInputStream *fileInputStream = g_file_read(inFile, NULL, &error);
-    if (error != NULL)
-    {
-        g_print("Error: %s\n", error->message);
-    }
-
-    g_print("start downloading file: %s\n", gtk_entry_get_text(GTK_ENTRY(gUrlEntry)));
-    char *inFileBasename = g_file_get_basename(inFile);
-    g_print("file basename: %s\n", inFileBasename);
-    g_object_unref(G_OBJECT(inFile));
-
-    GFile *outFile = g_file_new_for_path(inFileBasename);
-    GFileOutputStream *fileOutputStream = g_file_create(outFile, G_FILE_CREATE_NONE, NULL, &error);
-    gssize fileSize = g_output_stream_splice(
-        G_OUTPUT_STREAM(fileOutputStream),
-        G_INPUT_STREAM(fileInputStream),
-        G_OUTPUT_STREAM_SPLICE_CLOSE_SOURCE, NULL, &error);
-    g_print("File size: %d\n", (int)fileSize);
-
-    g_object_unref(G_OBJECT(outFile));
-    g_object_unref(G_OBJECT(fileInputStream));
-    g_object_unref(G_OBJECT(fileOutputStream));
-}
-
-void perform_download_by_copying()
-{
-    GFile *inFile = g_file_new_for_uri(gtk_entry_get_text(GTK_ENTRY(gUrlEntry)));
-    char *inFileBasename = g_file_get_basename(inFile);
-    GFile *outFile = g_file_new_for_path(inFileBasename);
-
-    g_file_copy_async(inFile, outFile, G_FILE_COPY_BACKUP, G_PRIORITY_DEFAULT, NULL, download_progress, NULL, download_finished, NULL);
-}
-
-void download_progress(goffset currentNumBytes, goffset totalNumBytes, gpointer userData)
-{
-    gtk_progress_bar_set_fraction(GTK_PROGRESS_BAR(gProgressBar), (gdouble)currentNumBytes / (gdouble)totalNumBytes);
-    g_print("\rProgress: %f%%",(gdouble)((gdouble)currentNumBytes) / (gdouble)totalNumBytes * 100);
-}
-
-void download_finished(GObject *sourceObject, GAsyncResult *result, gpointer userData)
-{
-    printf("\nDownloading is finished\n");
 }
